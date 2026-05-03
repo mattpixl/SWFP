b6c6e8484e2bd6fada37f2a47ff60d07d0b1e631
mattpixl <50707630+mattpixl@users.noreply.github.com>
2026-05-03 19:42:51 -0400

    Delete SW-Final-Project/FinalProject directory

--- SW-Final-Project/FinalProject/api/models/promotion.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import Column, String, Date
-from api.dependencies.database import Base
-
-class Promotion(Base):
-    __tablename__ = "promotion"
-
-    promotionCode = Column(String(10), primary_key=True, index=True)
-    promotionExpire = Column(Date, nullable=False)
